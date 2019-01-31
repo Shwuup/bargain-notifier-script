f5257d24ba11878173b8b550a9b134d361450bd7
Shwuup <e_kong98z@hotmail.com>
2019-02-01 01:05:26 +1100

    can notify now

--- notifier.py
+++ notifier.py
@@ -1,13 +1,30 @@
 from twilio.rest import Client
 import requests
+import argparse 
 from bs4 import BeautifulSoup
+import re
+from pathlib import Path
 
-with open("secrets.txt") as file:
-    secrets = file.read().strip().split("\n")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', nargs='*')
+    args = parser.parse_args()
 
-twilioCli = Client(secrets[0], secrets[1])
+    with open(str(Path.home()) + "/secrets.txt") as file:
+        secrets = file.read().strip().split("\n")
+    twilioCli = Client(secrets[0], secrets[1])
+    c = requests.get("https://www.ozbargain.com.au/").content
+    soup = BeautifulSoup(c)
 
-# message = twilioCli.messages.create(body="how you doing?", 
-#                                     from_=secrets[2], 
-#                                     to=secrets[3])
+    res = soup.find_all("h2", class_="title")
+    for offers in res:
+        for search_term in args.i:
+            if search_term.lower() in offers['data-title'].lower():
+                link = "https://www.ozbargain.com.au/node/" + offers['id'].strip("title")
+                messageBody=("Bargain for %s: %s" % (search_term,link))
+                twilioCli.messages.create(body=messageBody,
+                                    from_=secrets[2], 
+                                    to=secrets[3])
 
+if __name__=='__main__':
+    main()
