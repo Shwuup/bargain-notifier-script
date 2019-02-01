e4bbad35e8c58eec372c9fc682f21f5a12ae0b94
Shwuup <e_kong98z@hotmail.com>
2019-02-01 13:22:22 +1100

    add persistent storage don't notify seen deals

--- notifier.py
+++ notifier.py
@@ -2,14 +2,15 @@ from twilio.rest import Client
 import requests
 import argparse 
 from bs4 import BeautifulSoup
-import re
 from pathlib import Path
+import shelve
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', nargs='*')
     args = parser.parse_args()
 
+    already_seen = shelve.open("seen_deals")
     with open(str(Path.home()) + "/secrets.txt") as file:
         secrets = file.read().strip().split("\n")
     twilioCli = Client(secrets[0], secrets[1])
@@ -19,12 +20,15 @@ def main():
     res = soup.find_all("h2", class_="title")
     for offers in res:
         for search_term in args.i:
-            if search_term.lower() in offers['data-title'].lower():
-                link = "https://www.ozbargain.com.au/node/" + offers['id'].strip("title")
-                messageBody=("Bargain for %s: %s" % (search_term,link))
+            link = "https://www.ozbargain.com.au/node/" + offers['id'].strip("title")
+            if search_term.lower() in offers['data-title'].lower() and link not in already_seen:
+                already_seen[link] = offers['data-title']
+                messageBody=("%s\n%s" % (offers['data-title'],link))
                 twilioCli.messages.create(body=messageBody,
                                     from_=secrets[2], 
                                     to=secrets[3])
+                print("link sent: " + link)
+    already_seen.close()
 
 if __name__=='__main__':
     main()
