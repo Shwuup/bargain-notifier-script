beb77ac37f1043b3162e2cad95ee7bc4232bad73
Shwuup <e_kong98z@hotmail.com>
2019-04-02 19:56:02 +1100

    add email notifications

--- notifiers.py
+++ notifiers.py
@@ -24,15 +24,27 @@ class TextNotifier(Notifier):
             return secrets
 
     def send_message(self):
-        secrets = self.read_secrets()
-        twilioCli = Client(secrets[0], secrets[1])
-        twilioCli.messages.create(body=self.message, from_=secrets[2], to=secrets[3])
+        account_sid, auth_token, twilio_phone_number, your_phone_number = (
+            self.read_secrets()
+        )
+        twilioCli = Client(account_sid, auth_token)
+        twilioCli.messages.create(
+            body=self.message, from_=twilio_phone_number, to=your_phone_number
+        )
 
 
 class EmailNotifier(Notifier):
     def read_secrets(self):
-        pass
+        with open(os.getcwd() + "/email_secrets.txt") as file:
+            secrets = file.read().strip().split("\n")
+        return secrets
 
     def send_message(self):
-        pass
+        email, password = self.read_secrets()
+        smtpObj = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        smtpObj.login(email, password)
+        smtpObj.sendmail(
+            email, email, "Subject: OzBargain Notification!\n" + self.message
+        )
+        smtpObj.quit()
 
